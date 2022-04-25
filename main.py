5296e4266f3d4235e1b5a7e71c7eae9e8a6dd33a
unknown <lfrackowiak4@gmail.com>
2022-04-25 19:10:33 +0200

    druga

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from math import factorial
 def binom(n, k):
-	return factorial(n)/(factorial(k)*factorial(n-k))
+	return factorial(n)//(factorial(k)*factorial(n-k))
 n,k = map(int, input().split())
 print(binom(n,k))
